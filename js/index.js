1b20482716bc1a7b9b77fa5901ebc696b2817341
crystal <972369976@qq.com>
2017-02-21 21:24:47 +0800

    index page

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$('.masthead').css('height',mastheadHeight);
 	//背景图随屏幕大小改变
 	$(window).resize(function(){
-		var mastheadHeight=Math.ceil($('#assitImg').css('height'));
+		var mastheadHeight=$('#assitImg').css('height');
 		$('.masthead').css('height',mastheadHeight);
 	});
 	
