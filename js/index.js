0209ae5e575184e4a4076a5868f05045732c13ec
crystal <972369976@qq.com>
2017-02-22 19:53:03 +0800

    解决出现横向滚动条的问题

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
+	//初始化背景图大小
 	var mastheadHeight=$('#assitImg').css('height');
-	$('.masthead').css('height',mastheadHeight);
+	mastheadHeight=parseInt(mastheadHeight);
+	mastheadHeight+=7;
+	console.log(mastheadHeight);
+	$('.masthead').css('height',mastheadHeight+'px');
 	//背景图随屏幕大小改变
 	$(window).resize(function(){
 		var mastheadHeight=$('#assitImg').css('height');
+		mastheadHeight=parseInt(mastheadHeight);
+		mastheadHeight+=7;
 		$('.masthead').css('height',mastheadHeight);
+		console.log(typeof(mastheadHeight)+mastheadHeight);
 	});
 	
 });
